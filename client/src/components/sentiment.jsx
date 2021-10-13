c715e492d17c6a7ae9a5a7ffc958b269fb06ebe6
Matt Weir <n10509020@qut.edu.au>
2021-10-13 18:23:26 +1000

    loading states

--- client/src/components/sentiment.jsx
+++ client/src/components/sentiment.jsx
@@ -1,11 +1,33 @@
-export default function Sentiment(){
-    return(
-        <div className="ml-4 border-gray-200 border shadow-md rounded-xl h-full w-full bg-gray-200 bg-opacity-25">
-<div className="w-full bg-blue-200 rounded-t-xl">
-            <h1 className="text-center">Sentiment</h1>
+import { useState } from "react"
 
+export default function Sentiment() {
+
+    const [loading, setLoading] = useState(true);
+
+    if (loading) {
+
+        return (
+            <div className="ml-4 border-gray-200 border shadow-md rounded-xl h-full w-full bg-gray-200 bg-opacity-25">
+                <div className="w-full bg-blue-200 rounded-t-xl">
+                    <h1 className="text-center">Sentiment</h1>
+
+                </div>
+                <div className="flex justify-center items-center mt-20">
+                    <button className="bg-white rounded-xl p-2 shadow animate-bounce">Loading...</button>
+                </div>
             </div>
-        </div>
-    )
+        )
+    }
+    else {
+        return (
+            <div className="ml-4 border-gray-200 border shadow-md rounded-xl h-full w-full bg-gray-200 bg-opacity-25">
+                <div className="w-full bg-blue-200 rounded-t-xl">
+                    <h1 className="text-center">Sentiment</h1>
+
+                </div>
+            </div>
+        )
+    }
+
 
 }
--- client/src/components/twitterFeed.jsx
+++ client/src/components/twitterFeed.jsx
@@ -1,22 +1,45 @@
+import { useState } from "react"
 import Tweets from "./tweetTest"
 
 export default function TwitterFeed(){
 
-    
+    const [loading, setLoading] = useState(false);
 
-    return(
+
+    if(loading){
+        return (
+            <div   className="mr-4 border-gray-200 border shadow-md rounded-t-xl w-2/5 pb-6">
+                 <div className="w-full bg-blue-200 rounded-t-xl">
+                <h1 className="text-center">Live Feed</h1>
+                    
+                </div>
+                <div  id="live-feed" className="h-full overflow-y-scroll bg-gray-200 bg-opacity-25">
+                <div className="flex justify-center items-center mt-20">
+                    <button className="bg-white rounded-xl p-2 shadow animate-bounce">Loading...</button>
+                </div>
+                    </div>
+            </div>
+            
+        )
+    }
+    else{
+        return(
         
        
-        <div   className="mr-4 border-gray-200 border shadow-md rounded-t-xl w-2/5 pb-6">
-             <div className="w-full bg-blue-200 rounded-t-xl">
-            <h1 className="text-center">Live Feed</h1>
-                
-            </div>
-            <div  id="live-feed" className="h-full overflow-y-scroll bg-gray-200 bg-opacity-25">
-            <Tweets />
+            <div   className="mr-4 border-gray-200 border shadow-md rounded-t-xl w-2/5 pb-6">
+                 <div className="w-full bg-blue-200 rounded-t-xl">
+                <h1 className="text-center">Live Feed</h1>
+                    
                 </div>
-        </div>
-    )
+                <div  id="live-feed" className="h-full overflow-y-scroll bg-gray-200 bg-opacity-25">
+                <Tweets />
+                    </div>
+            </div>
+        )
+    }
+
+
+    
 }
 
 
