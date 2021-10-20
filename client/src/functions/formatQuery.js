6a6568459dd838a2a3ca3ad1b13e73d0cab9e0a7
Matt Weir <n10509020@qut.edu.au>
2021-10-20 17:27:10 +1000

    changing query formatter

--- client/src/functions/formatQuery.js
+++ client/src/functions/formatQuery.js
@@ -1,10 +1,9 @@
 export default function FormatQuery(hashtags){
   if(hashtags){
-    var query = " ";
+    var query = "";
     for(var i = 0; i< hashtags.length; i++) {
-      query.concat(hashtags[i] + " ,");
+      query.concat(hashtags[i] + " ");
     }
-
     return query;
   }
     
