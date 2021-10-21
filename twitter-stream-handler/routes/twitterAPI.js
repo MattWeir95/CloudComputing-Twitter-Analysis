758dbffd2760fa70268a1e4ac273bf5c2263d907
Matt Weir <n10509020@qut.edu.au>
2021-10-21 17:27:59 +1000

    setting up redis

--- twitter-stream-handler/routes/twitterAPI.js
+++ twitter-stream-handler/routes/twitterAPI.js
@@ -4,13 +4,21 @@ var Twitter = require('twitter');
 var router = express.Router();
 const https = require("https");
 const dotenv = require('dotenv/config');
+const redis = require('redis');
 
 const apiKey= process.env.TWITTER_API_KEY;
 const apiSecretKey= process.env.TWITTER_API_KEY_SECRET;
 const accessToken = process.env.TWITTER_ACCESS_TOKEN;
 const accessTokenSecret=process.env.TWITTER_ACCESS_TOKEN_SECRET;
-const show_re_tweets = false;
 
+//Create redis client and check for errors
+const redisClient = redis.createClient();
+redisClient.on('error', (err) =>  {
+    console.log("Error " + err  ); 
+});  
+
+
+//Create twitter api client
 var client = new Twitter({
     consumer_key: apiKey,
     consumer_secret: apiSecretKey,
@@ -19,22 +27,26 @@ var client = new Twitter({
   });
 
 
-  //New querys are appended to track, seperated by a comma,
-  //We can use follow: "comma seperate list of user ids" to filter by user
+//New querys are appended to track, seperated by a comma,
+//We can use follow: "comma seperate list of user ids" to filter by user
   
 const params = {
   track: "twitter, ben, thomas",
   language: "en"
 }
 
+
+
 router.get("/:query", function (req, res, nex) {
-    var userInput = req.params
+    // var userInput = req.params
     // console.log(userInput);
     // console.log(apiKey);
+
+    //Get stream of tweets
     client.stream('statuses/filter', params,  function(stream) {
         stream.on('data', function(tweet) {
-          // res.status(200).send(tweet);
-          console.log(tweet);
+          console.log(tweet.text);
+       
         });
       
         stream.on('error', function(error) {
