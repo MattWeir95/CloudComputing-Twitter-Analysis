d7f340a308bb1baf91bc6cce09c35343466ba7c1
Matt Weir <n10509020@qut.edu.au>
2021-10-21 11:10:19 +1000

    Comments/format

--- client/src/App.js
+++ client/src/App.js
@@ -7,44 +7,47 @@ import Hashtags from "./components/hashtags";
 import TwitterFeed from "./components/twitterFeed";
 import Sentiment from "./components/sentiment";
 function App() {
+
   const [hashtags, setHashtags] = useState([]);
+
+  //Switches screen between homepage & history page
   const [view, setView] = useState("homepage");
 
+  //Tweet that has been selected from the twitter feed
   const [selectedTweet, setSelectedTweet] = useState(null);
 
-  if(view === "homepage"){
+  if (view === "homepage") {
     console.log(selectedTweet)
     return (
       <div className="h-screen">
-        <HeaderLogo setSelectedTweet={setSelectedTweet} setHashtags={setHashtags} setView={setView} view={view}/>
-  
+        <HeaderLogo setSelectedTweet={setSelectedTweet} setHashtags={setHashtags} setView={setView} view={view} />
+
         <div className="border border-gray-200 rounded-xl mt-5 mx-10 shadow-xl pb-5">
           <SearchBar hashtags={hashtags} setHashtags={setHashtags} />
-  
+
           <Hashtags hashtags={hashtags} setHashtags={setHashtags} />
           <SearchButton hashtags={hashtags} />
         </div>
-  
-        <div className="h-1/2 flex flex-row mt-5 mx-10"> 
-        <TwitterFeed setSelectedTweet={setSelectedTweet}/>
-        
-        <Sentiment selectedTweet={selectedTweet}/>
-        
+
+        <div className="h-1/2 flex flex-row mt-5 mx-10">
+          <TwitterFeed setSelectedTweet={setSelectedTweet} />
+
+          <Sentiment selectedTweet={selectedTweet} />
+
         </div>
       </div>
     );
   }
 
-  if(view === "history"){
-    return(
-<div className="h-screen">
-        <HeaderLogo setHashtags={setHashtags} setView={setView} view={view}/>
-  
-        
+  if (view === "history") {
+    return (
+      <div className="h-screen">
+        <HeaderLogo setHashtags={setHashtags} setView={setView} view={view} />
+
       </div>
     );
   }
-  
+
 }
 
 export default App;
