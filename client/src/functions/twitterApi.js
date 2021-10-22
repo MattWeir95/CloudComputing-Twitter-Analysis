0b670633279cb0f26e5535568b8e5d5063e0b656
Matt Weir <n10509020@qut.edu.au>
2021-10-22 19:43:04 +1000

    stop sending empty queries

--- client/src/functions/twitterApi.js
+++ client/src/functions/twitterApi.js
@@ -3,7 +3,8 @@ export default function SendTwitterQuery(hashtags){
     const SERVER_PORT = 3004;
     const API_URL =  `http://localhost:${SERVER_PORT}/api/`;
 
-    if(hashtags){
+    if(hashtags && hashtags.length > 0){
+        
         var query = hashtags.join(', ');
 
         fetch(API_URL + query)
