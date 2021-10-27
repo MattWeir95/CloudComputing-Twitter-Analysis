de16140339b503e93cfa425e6281b6d75589569d
Matt Weir <n10509020@qut.edu.au>
2021-10-27 18:58:59 +1000

    refactor

--- client-server/server.js
+++ client-server/server.js
@@ -38,9 +38,9 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(cors())
 
-app.get('/:id', (req,res,next) => {
+app.get('/:id', (req, res, next) => {
   res.setHeader('Content-Type', 'application/json');
-  redisClient.get(`tweet:${req.params.id}`,(err,tweet) => { res.send(tweet); })
+  redisClient.get(`tweet:${req.params.id}`, (err, tweet) => { res.send(tweet); })
 })
 
 http.listen(PORT, () => {
@@ -53,7 +53,7 @@ var c_idx;
 io.on('connection', (socket) => {
   // console.log('a user connected', socket.id);
   sockets[socket.id] = socket;
-  socket.emit('connection',null);
+  socket.emit('connection', null);
   socket.hashtags = [];
   socket.on('hashtags', new_ht => {
     console.log(new_ht);
@@ -79,7 +79,7 @@ io.on('connection', (socket) => {
 
 async function getKey(key) {
   return new Promise(resolve => {
-    redisClient.get(key, (err,res) => {
+    redisClient.get(key, (err, res) => {
       resolve(res);
     })
   })
@@ -94,30 +94,38 @@ setInterval(async () => {
     await getKey(key).then(res => {
       if (res === null) { return false; }
       tweet = JSON.parse(res).tweet;
-      if(tweet.text){
+      if (tweet.text) {
         var tweet_text = tweet.text.toLowerCase();
         for (var id in sockets) {
           socket = sockets[id];
-          
 
 
           //We also need a better way at deciding if a tweet is matched, would be nice if we could completely ignore retweets/replies ect in the stream
-          
+
           //For some reason it is sending the match however the front end isnt catching it, it is also sending the history,
           //however the history is being caught and displayed.
           //If you take them out of the if/else and just socket.emit('match',tweet); it works, something is happening here,
           //that its only allowing or letting one of the emits work at once.
           //FFS
-          if (socket.hashtags.some((hashtag) => { hashtag = hashtag.toLowerCase(); return tweet_text.includes(hashtag); })) {
-            socket.emit('match', tweet)
-          } else {
-            socket.emit('history', tweet)
+          var hashtag_matches_tweet = socket.hashtags.some(hashtag => {
+            hashtag = hashtag.toLowerCase();
+            return tweet_text.includes(hashtag);
+          })
+          
+            if (hashtag_matches_tweet) {
+              socket.emit('match', tweet)
+              console.log("Match")
+            } else {
+              socket.emit('history', tweet)
+              console.log("History")
+            }
+        
 
-          }
+          
         }
-      return true;
+        return true;
       }
-      
+
     }).then(res => {
       if (res) { c_idx++; }
       found_more = res;
@@ -125,9 +133,11 @@ setInterval(async () => {
   } while (found_more == true);
 
 }
-//Put timeout here to slowdown feed, i have it off here atm so we dont miss out on any tweets and can see
-//how fast it is, maybe we should slow it down and batch them? or just leave them as it is and pick slower tweets
+  //Put timeout here to slowdown feed, i have it off here atm so we dont miss out on any tweets and can see
+  //how fast it is, maybe we should slow it down and batch them? or just leave them as it is and pick slower tweets
 );
+
+
 
 
 
